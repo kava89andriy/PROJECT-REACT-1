02bcad0b837168841f66faed04efdf5a95fe0469
Andrii Kavetskyi <kava89andriy@gmail.com>
2022-11-23 09:57:05 +0100

    Project

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -1,34 +1,32 @@
 import React from "react";
 
 const Services = () => {
-    const servicesList = [
-        {
-            name: "Usługa 1",
-            info: "nowość"
-        },
-        {
-            name: "Usługa 2",
-            info: ""
-        },
-        {
-            name: "Usługa 3",
-            info: ""
-        },
-        {
-            name: "Usługa 4",
-            info: ""
-        },
-        {
-            name: "Usługa 5",
-            info: ""
-        },
-        {
-            name: "Usługa 6",
-            info: ""
-        },
-     
-    ]
-
+  const servicesList = [
+    {
+      name: "Usługa 1",
+      info: "nowość",
+    },
+    {
+      name: "Usługa 2",
+      info: "",
+    },
+    {
+      name: "Usługa 3",
+      info: "",
+    },
+    {
+      name: "Usługa 4",
+      info: "",
+    },
+    {
+      name: "Usługa 5",
+      info: "",
+    },
+    {
+      name: "Usługa 6",
+      info: "",
+    },
+  ];
 
   return (
     <section className="services" id="services">
@@ -36,18 +34,14 @@ const Services = () => {
         <div className="section-title">
           <h2>Czym zajmuje się nasza firma?</h2>
         </div>
-        {
-                servicesList.map((item, index) => {
-                    return  <div className="offer-wrapper">
-                    <div className="offer-item">
-                      <h3>{item.name}</h3>
-                      <span>{item.info}</span>
-                    </div>
-                    
-                  </div>
-                } )
-        }
-       
+        <div className="offer-wrapper">
+          {servicesList.map((item, index) => {
+            return <div className="offer-item">
+              <h3>{item.name}</h3>
+              <span>{item.info}</span>
+            </div>;
+          })}
+        </div>
       </div>
     </section>
   );
